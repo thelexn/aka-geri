a8a89270033dd71d2e409d912e7e30a769627c50
Alex <lohn.alexander@gmail.com>
2017-02-12 18:10:07 +0100

    no message

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,29 +1,30 @@
 $(function() {
 	smoothScroll(500);
-
-	$(window).on("load resize", function() {
-		// console.log(window.innerHeight + "-" + window.innerWidth);
-		$(".header-wrap").css("height", window.innerHeight);
-		if (window.innerWidth > 575){
-			$("#headline").css("top", window.innerHeight - 215);
-		} else {
-			$("#headline").css("top", window.innerHeight - 255);
-		}
-	});
+	adjustBackgroud(575);
 
 });
 
 function smoothScroll (duration) {
 	$('a[href^="#"]').on('click', function(event) {
 
-	    var target = $( $(this).attr('href') );
+    var target = $( $(this).attr('href') );
 
-	    if( target.length ) {
-	        event.preventDefault();
-	        $('html, body').animate({
-	            scrollTop: target.offset().top
-	        }, duration);
-	    }
-	});
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').animate({ scrollTop: target.offset().top }, duration);
+    }
+	}
+});
 
-}
+function adjustBackgroud (breakpoint) {
+	$(window).on("load resize", function() {
+		//set BG-Img full display-height
+		$(".header-wrap").css("height", window.innerHeight);
+
+		//place Headline on the bottom of the front-page
+		if (window.innerWidth > breakpoint){
+			$("#headline").css("top", window.innerHeight - 215);
+		} else {
+			$("#headline").css("top", window.innerHeight - 255);
+		}
+});

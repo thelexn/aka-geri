fcf6d38292873c3b5bcd5359c587380a219052e7
Alex <lohn.alexander@gmail.com>
2017-04-22 18:57:50 +0200

    repair smooth scrolling - defect after building imressum paths

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -15,15 +15,16 @@ $(function() {
 
 function smoothScroll (duration) {
 	$('a[href^="#"]').on('click', function(event) {
+		// console.log("es wurde geklickt auf " + event);
 
-	    var target = $( $(this).attr('href') );
+    var target = $( $(this).attr('href') );
 
-	    if( target.length ) {
-	        event.preventDefault();
-	        $('html, body').animate({
-	            scrollTop: target.offset().top
-	        }, duration);
-	    }
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, duration);
+    }
 	});
 
 }
